f61717b479b352b0250672225a51447710601753
sagimadai <madsagi11@gmail.com>
2025-03-30 08:55:22 +0000

    Added ping results file to the script

--- pingstorm.sh
+++ pingstorm.sh
@@ -3,6 +3,12 @@
 #list of websites to check
 SERVICES=("google.com" "facebook.com" "youtube.com" "tiktok.com" "netflix.com")
 
+#results file name
+output_file="ping_results.txt"
+
+> "$output_file"
+
+
 echo "PingStorm - Internet Latency time Checker"
 echo "---------------------------------------"
 
@@ -12,9 +18,9 @@ for service in "${SERVICES[@]}";do
 	avg_time=$(ping -c 5 "$service" | awk -F'/' 'END {print $5}')
 
 	if [ -z "$avg_time" ]; then
-		echo "$service is unreachable"
+		echo "$service is unreachable" >> "$output_file"
 	else
-		echo "$service - Average latency: $avg_time ms"
+		echo "$service - Average latency: $avg_time ms" >> "$output_file"
 		fi
 	done
-
+echo "ping results saved to file $output_file"
